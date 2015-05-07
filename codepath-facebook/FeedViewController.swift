e31cfa983830e1f934269305589ba3c4835be61e
Patrick Dugan <dugan.pat@gmail.com>
2015-05-06 23:38:16 -0700

    initial commit
    initial codepath facebook commit

---
+++ codepath-facebook/FeedViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FeedViewController.swift
+//  codepath-google
+//
+//  Created by Patrick Dugan on 5/6/15.
+//  Copyright (c) 2015 dailydoog. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+        scrollView.contentSize = imageView.image!.size
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
